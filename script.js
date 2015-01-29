6d8400721d25f38cd58e1ce7ff1c0db0f0c8562f
Rocio Sevilla <rociosev@live.unc.edu>
2015-01-29 08:37:02 -0500

    adding files from class

---
+++ script.js
@@ -0,0 +1,18 @@
+// intro to Javascript
+
+var userName = "Rocio";
+var todaysDate = new Date();
+
+function myfunction(){
+document.write("Hello ");
+document.write(userName);
+console.log(todaysDate);
+}
+document.getElementById("my own").innerHTML="yup";
+document.getElementById("demo").innerHTML="This is the new info";
+
+var myNumberVariable = 5;
+var myTextVariable= "my variable is a string";
+myfunction();
+console.log(myNumberVariable);
+console.log(myTextVariable);
---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+var greeting = "Hello"; 
+var myName = "Rocio";
+var todaysDate = new Date();
+
+function myFunction(){
+document.write("Hello ");
+document.write(myName + "<br/>" + todaysDate);
+
+}
+
+myFunction();
+
+
